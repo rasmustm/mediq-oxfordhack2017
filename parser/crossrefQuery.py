9406b6b492ffac49000de95f12f66ff581471e49
RasmusTM <rasmustm@me.com>
2017-11-25 21:07:58 +0000

    Adding basic value function

--- parser/crossrefQuery.py
+++ parser/crossrefQuery.py
@@ -1,7 +1,7 @@
 from habanero import Crossref
 
 def crossrefQuery(query):
-	# takes a word, then searches and returns a 2-D list with each DOI and title
+	# returns a list of DOIs
 
 	cr = Crossref()
 	queryResults = cr.works(query=query)
@@ -9,6 +9,6 @@ def crossrefQuery(query):
 	cleanResults = []	
 
 	for e in queryResults["message"]["items"]:
-		cleanResults.append([e["title"][0], e["DOI"]])
+		cleanResults.append(e["DOI"])
 
 	return cleanResults
--- parser/value_function_example.py
+++ parser/value_function_example.py
@@ -1,9 +1,12 @@
 from crossrefQuery import crossrefQuery
 
+# creates a dictionary mapping DOI -> value for each term
+
 term1 = "depression"
 a1 = 3
 
 term1_results = crossrefQuery(term1)
 term1_dictionary = {}
 
-for 
+for entry in term1_results:
+	term1_dictionary[entry[0] : a1 ]

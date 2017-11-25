910e362b4d13ca9980e9276de7b322adbc88972f
RasmusTM <rasmustm@me.com>
2017-11-25 17:44:10 +0000

    added some changes to the web server

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,13 +1,25 @@
 from flask import Flask, render_template, url_for, request, redirect
+from mendeley import Mendeley, resources
 
+# # Registering for Mendeley API
+# mendeley = Mendeley(4968, client_secret="YTjaHusKfWLU7tSk")
+# auth = mendeley.start_client_credentials_flow()
+# session = auth.authenticate()
+
+# Create flask application
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
-def index():
+def index(queryResults = None):
 	if request.method == "POST":
 		print("query:", request.form["query"])
-		return redirect(url_for("index"))
-	return render_template("index.html") 
+		query = request.form["query"]
+		
+		# # Search for relevant articles
+		# rawQueryResults = resources.catalog.Catalog.search(query).iter()
+
+		queryResults = "query"
+	return render_template("index.html", queryResults=queryResults) 
 @app.route("/test")
 def test():
 	return "test"

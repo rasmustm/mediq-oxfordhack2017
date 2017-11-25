6d6eaeb4cdf3257a1ad2ee20aba75deab0ad2fb9
RasmusTM <rasmustm@me.com>
2017-11-25 15:41:50 +0000

    Adding .gitignore, moved javascript to separate file

---
+++ python-flask/static/script.js
@@ -0,0 +1,31 @@
+function startDictation() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+      var recognition = new webkitSpeechRecognition();
+
+      recognition.continuous = false;
+      recognition.interimResults = false;
+
+      recognition.lang = "en-US";
+      recognition.start();
+
+      recognition.onresult = function(e) {
+        document.getElementById('transcript').value
+                                 = e.results[0][0].transcript;
+        recognition.stop();
+        // document.getElementById('test').submit();
+      };
+
+      recognition.onerror = function(e) {
+        recognition.stop();
+      }
+
+    }
+  }
+
+$(document).ready(function() {
+	$("#microphoneButton").click(function () {
+		$(this).toggleClass("red");
+	});
+});

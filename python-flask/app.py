bad62eb03365b7c4df485ab337b4e1e48f574c05
RasmusTM <rasmustm@me.com>
2017-11-25 15:13:34 +0000

    passing voice from javascript to python flask

--- python-flask/app.py
+++ python-flask/app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+	if request.method == "POST":
+		print("query:", request.form["query"])
+		return redirect(url_for("index"))
 	return render_template("index.html") 
 @app.route("/test")
 def test():
